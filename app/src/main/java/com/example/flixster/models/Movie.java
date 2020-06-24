89a0deba199e33edea2768b3e1d85cc3fa63bf64
Isaiah Suarez <isaiahsuarez@fb.com>
2020-06-24 10:26:50 -0700

    Added helper method to Movie.java to convert Json array to movies array.

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -1,7 +1,11 @@
 package com.example.flixster.models;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Movie {
 
@@ -13,5 +17,14 @@ public class Movie {
         this.posterPath = jsonObject.getString("poster_path");
         this.title = jsonObject.getString("title");
         this.overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+
+        return movies;
     }
 }
