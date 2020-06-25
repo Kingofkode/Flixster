b1de3ec596774b8c5e5f9a4a03a0af8004a4040c
Isaiah Suarez <isaiahsuarez@fb.com>
2020-06-25 13:46:16 -0700

    Implemented MovieDetailsActivity.

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -3,12 +3,44 @@ package com.example.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+    public static final String TAG = "MovieDetailsActivity";
+
+    // Movie to display
+    Movie movie;
+
+    // View objects
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        // Resolve the view objects
+        tvTitle = findViewById(R.id.tvTitle);
+        tvOverview = findViewById(R.id.tvOverview);
+        rbVoteAverage = findViewById(R.id.rbVoteAverage);
+
+        // Unwrap the movie passed via intent, using its simple name as a the key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d(TAG, String.format("Showing details for '%s", movie.getTitle()));
+
+        // Set title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+        // Vote average is 0 - 10, converted to 0 - 5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
     }
 }
