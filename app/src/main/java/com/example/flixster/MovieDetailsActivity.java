68f27fc088420b7e78a49f15224290d58020b60f
Isaiah Suarez <isaiahsuarez@fb.com>
2020-06-26 13:06:52 -0700

    Cleaned up unused code.

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -1,17 +1,16 @@
 package com.example.flixster;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.databinding.ActivityMovieDetailsBinding;
 import com.example.flixster.models.Movie;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,43 +19,31 @@ import org.parceler.Parcels;
 
 import okhttp3.Headers;
 
-public class MovieDetailsActivity extends AppCompatActivity {
+public class MovieDetailsActivity extends YouTubeBaseActivity {
 
     public static final String TAG = "MovieDetailsActivity";
 
     // Movie to display
     Movie movie;
-    String videoID;
-    public static final String VIDEO_ID_EXTRA = "videoID";
+
+    ActivityMovieDetailsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMovieDetailsBinding binding = ActivityMovieDetailsBinding.inflate(getLayoutInflater());
+        binding = ActivityMovieDetailsBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
-        getSupportActionBar().setTitle("Details");
-
         // Unwrap the movie passed via intent, using its simple name as a the key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
-        Log.d(TAG, String.format("Showing details for '%s", movie.getTitle()));
-
+        loadVideoForMovie();
         // Set title and overview
         binding.tvTitle.setText(movie.getTitle());
         binding.tvOverview.setText(movie.getOverview());
         // Vote average is 0 - 10, converted to 0 - 5 by dividing by 2
         float voteAverage = movie.getVoteAverage().floatValue();
         binding.rbVoteAverage.setRating(voteAverage > 0 ? voteAverage / 2.0f : voteAverage);
-        loadVideoForMovie();
-    }
-
-    public void onTrailerClick(View view) {
-        Intent movieTrailerIntent = new Intent(this, MovieTrailerActivity.class);
-        if (videoID != null) {
-            movieTrailerIntent.putExtra(VIDEO_ID_EXTRA, videoID);
-            this.startActivity(movieTrailerIntent);
-        }
 
     }
 
@@ -72,9 +59,8 @@ public class MovieDetailsActivity extends AppCompatActivity {
                 try {
                     JSONArray results = responseObject.getJSONArray("results");
                     if (results.getJSONObject(0) != null) {
-                        String key = results.getJSONObject(0).getString("key");
-                        videoID = key;
-                        Toast.makeText(MovieDetailsActivity.this, "Got key", Toast.LENGTH_SHORT).show();
+                        String videoID = results.getJSONObject(0).getString("key");
+                        launchPlayer(videoID);
                     }
                 } catch (JSONException e) {
                     Log.e(TAG, "JSON Exception", e);
@@ -87,6 +73,21 @@ public class MovieDetailsActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void launchPlayer(final String videoID) {
+        // Resolve player view from layout
+        binding.player.initialize(getString(R.string.youtube_api_key), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.cueVideo(videoID);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.e(TAG, "onInitializationFailure");
+            }
+        });
     }
 
 
